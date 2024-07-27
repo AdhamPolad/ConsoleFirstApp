fa2d83c6e5ddf88c82a79f30f7d41bc8db005f3f
AdhamPolad <edhempoladzade2@gmail.com>
2024-07-27 12:30:41 +0400

    add initial commit

---
+++ initialConsoleAppSln/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 6;
+            int num2 = 7;
+
+            int sum;
+            sum = num1 + num2;
+            Console.WriteLine("Result: " + sum);
+
+
+            int num3 = 4;
+            int num4 = 5;
+            int multiplication = num3 * num4;
+            Console.WriteLine("Multiplication: " + multiplication);
+        }
+    }
+}
+
+
---
+++ initialConsoleAppSln/initialConsoleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace initialConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Number section
+            byte byteType = 246;
+            short shortType = 30000;
+            int intType = 123001;
+            long longType = 12399392;
+
+
+
+
+
+            // Floating
+
+            float floatType = 2.3f;
+            double doubleType = 12.3;
+            decimal decimalType = 14.5m;   //daha deqiq olsun deye decimal daha cox istifade olunur
+
+
+
+            // Logical Type
+            bool boolType = true;
+
+
+            //Char
+            char charType = 'a';  //sadece 1 simvol 
+
+
+
+            //String
+            string name = "Adham";
+
+
+
+            //DateTime
+            DateTime dateTime = new DateTime(2024,07,24);
+
+
+
+            // Car
+            string carMark = "Kia";
+            string carModel = "Sorento";
+            DateTime releaseDate = new DateTime(2010, 01, 01);
+            string carColor = "Gold";
+            string carType = "Suv";
+            byte carEngine = 2;
+            int carDistance = 255000;
+            bool hasTurbo = true;
+            int carValue = 26000;
+            byte carSits = 7;
+
+
+
+            // Person
+            string personName = "Adham";
+            string personSurname = "Poladzadeh";
+            byte personAge = 20;
+            decimal personHeight = 1.83m;
+            DateTime personDate = new DateTime(2004, 06, 24);
+            byte personWeight = 74;
+            bool hasDrivingLisence = true;
+            string personPhoneNumber = "050-311-18-68";
+            string personAddress = "Mehemmed Hadi 76";
+
+
+
+
+        }
+    }
+}
